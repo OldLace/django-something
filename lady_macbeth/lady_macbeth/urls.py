d20c109ac7c4f38c2bce23977887ce6081794cf2
Paul Gelot <pgelot@gmail.com>
2019-01-31 20:01:13 -0500

    django app created

--- lady_macbeth/lady_macbeth/urls.py
+++ lady_macbeth/lady_macbeth/urls.py
@@ -1,4 +1,4 @@
-"""super_hangman URL Configuration
+"""lady_macbeth URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
